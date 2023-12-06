4146261ed355ecf509b3a5aaeab832d2b01626e8
Quixote <quixotic1023@163.com>
2023-12-06 20:43:58 +0800

    feature：初始化短链接项目

---
+++ project/src/main/java/com/nageoffer/shortlink/project/config/MyMetaObjectHandler.java
@@ -0,0 +1,27 @@
+package com.nageoffer.shortlink.project.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        this.strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        this.strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+    }
+}
