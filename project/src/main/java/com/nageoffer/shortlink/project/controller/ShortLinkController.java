c86d922efde130443726815289deae0eae2d58a8
Quixote <quixotic1023@163.com>
2023-12-07 13:54:43 +0800

    feature：开发短链接分页查询功能

--- project/src/main/java/com/nageoffer/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/nageoffer/shortlink/project/controller/ShortLinkController.java
@@ -1,11 +1,15 @@
 package com.nageoffer.shortlink.project.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.nageoffer.shortlink.project.common.convention.result.Result;
 import com.nageoffer.shortlink.project.common.convention.result.Results;
 import com.nageoffer.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import com.nageoffer.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import com.nageoffer.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 import com.nageoffer.shortlink.project.service.ShortLinkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +28,14 @@ public class ShortLinkController {
      */
     @PostMapping("/api/short-link/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
-        shortLinkService.createShortLink(requestParam);
-        return Results.success(null);
+        return Results.success(shortLinkService.createShortLink(requestParam));
+    }
+
+    /**
+     * 分页查询短链接
+     */
+    @GetMapping("/api/short-link/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return Results.success(shortLinkService.pageShortLink(requestParam));
     }
 }
--- project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortLinkDO.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/ShortLinkDO.java
@@ -56,6 +56,11 @@ public class ShortLinkDO extends BaseDO {
     private String gid;
 
     /**
+     * 网站图像链接标识
+     */
+    private String favicon;
+
+    /**
      * 启用标识（0：启用，1：未启用）
      */
     private Integer enableStatus;
