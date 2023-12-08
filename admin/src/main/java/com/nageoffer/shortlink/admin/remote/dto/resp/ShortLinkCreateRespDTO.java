95480ebd1eca4da6109ad59d21ebc08d5d69455d
Quixote <quixotic1023@163.com>
2023-12-08 12:07:19 +0800

    feature：短链接后管系统调用中台请求相关接口

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/resp/ShortLinkCreateRespDTO.java
@@ -0,0 +1,32 @@
+package com.nageoffer.shortlink.admin.remote.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 短链接创建响应对象
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkCreateRespDTO {
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+}
