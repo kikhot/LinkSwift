d876350b3bc7da4ad88cfc33541c9a94d1ec6a5d
Quixote <quixotic1023@163.com>
2023-12-09 21:37:43 +0800

    feature：短链接后管开发短链接修改功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -1,5 +1,6 @@
 package com.nageoffer.shortlink.admin.remote;
 
+import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.TypeReference;
@@ -7,6 +8,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
@@ -32,6 +34,15 @@ public interface ShortLinkRemoteService {
         });
     }
 
+    /**
+     * 修改短链接
+     *
+     * @param requestParam 分组短链接总量请求参数
+     * @return 查询分组短链接总量响应
+     */
+    default void updateShortLink(ShortLinkUpdateReqDTO requestParam) {
+        HttpRequest.put("http://127.0.0.1:8001/api/short-link/v1/update").form(JSON.toJSONString(requestParam)).execute().body();
+    }
 
     /**
      * 分页查询短链接
@@ -63,4 +74,5 @@ public interface ShortLinkRemoteService {
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
         });
     }
+
 }
