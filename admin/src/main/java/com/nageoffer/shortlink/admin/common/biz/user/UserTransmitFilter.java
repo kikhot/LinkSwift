e8ac0549956e2fae8137a11b2bc9364de77457fb
Quixote <quixotic1023@163.com>
2023-12-11 14:35:51 +0800

    feature: 开发获取短链接目标网站标题接口

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -29,7 +29,7 @@ public class UserTransmitFilter implements Filter {
 
     private static final List<String> IGNORE_URI = Lists.newArrayList(
             "/api/short-link/admin/v1/user/login",
-            "/api/short-link/admin/v1/user/has-username",
+            "/api/short-link/admin/v1/user/has-username"
     );
 
     @SneakyThrows
