1671dcfed93bec8208b6a4bc184119f2ebf6a332
Quixote <quixotic1023@163.com>
2023-12-11 12:54:49 +0800

    docs: 短链接跳转原始链接添加方法注释

--- project/src/main/java/com/nageoffer/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/nageoffer/shortlink/project/controller/ShortLinkController.java
@@ -26,6 +26,9 @@ public class ShortLinkController {
 
     private final ShortLinkService shortLinkService;
 
+    /**
+     * 短链接跳转原始链接
+     */
     @GetMapping("/{short-uri}")
     public void restoreUrl(@PathVariable("short-uri") String shortUri, HttpServletRequest request, HttpServletResponse response) {
         shortLinkService.restoreUrl(shortUri, request, response);
