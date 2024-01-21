d80792bbffb776b40e92d7fd7e17be019412d72c
Quixote <1363762291@qq.com>
2024-01-21 14:31:47 +0800

    fix：修复serivce没有导入bean情况

--- project/src/main/java/com/nageoffer/shortlink/project/service/impl/LinkStatsTodayServiceImpl.java
+++ project/src/main/java/com/nageoffer/shortlink/project/service/impl/LinkStatsTodayServiceImpl.java
@@ -4,9 +4,11 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nageoffer.shortlink.project.dao.entity.LinkStatsTodayDO;
 import com.nageoffer.shortlink.project.dao.mapper.LinkStatsTodayMapper;
 import com.nageoffer.shortlink.project.service.LinkStatsTodayService;
+import org.springframework.stereotype.Service;
 
 /**
  * 短链接今日统计接口实现层
  */
+@Service
 public class LinkStatsTodayServiceImpl extends ServiceImpl<LinkStatsTodayMapper, LinkStatsTodayDO> implements LinkStatsTodayService {
 }
