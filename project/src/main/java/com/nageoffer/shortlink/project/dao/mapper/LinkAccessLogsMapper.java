aefa9df19659ec4dabc4f09c1a221d20d1142212
Quixote <1363762291@qq.com>
2024-01-21 10:46:46 +0800

    feature: 完善分组用户信息是否新老访客

--- project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
@@ -37,6 +37,24 @@ public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
     List<HashMap<String, Object>> listTopIpByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
 
     /**
+     * 根据分组获取指定日期内高频访问IP数据
+     */
+    @Select("SELECT " +
+            "    ip, " +
+            "    COUNT(ip) AS count " +
+            "FROM " +
+            "    t_link_access_logs " +
+            "WHERE " +
+            "    gid = #{param.gid} " +
+            "    AND create_time BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    gid, ip " +
+            "ORDER BY " +
+            "    count DESC " +
+            "LIMIT 5;")
+    List<HashMap<String, Object>> listTopIpByGroup(@Param("param") ShortLinkGroupStatsReqDTO requestParam);
+
+    /**
      * 根据短链接获取指定日期内新旧访客数据
      */
     @Select("SELECT " +
@@ -55,23 +73,6 @@ public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
             "        user " +
             ") AS user_counts;")
     HashMap<String, Object> findUvTypeCntByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
-
-    /**
-     * 根据短链接获取指定日期内PV、UV、UIP数据
-     */
-    @Select("SELECT " +
-            "    COUNT(user) AS pv, " +
-            "    COUNT(DISTINCT user) AS uv, " +
-            "    COUNT(DISTINCT ip) AS uip " +
-            "FROM " +
-            "    t_link_access_logs " +
-            "WHERE " +
-            "    full_short_url = #{param.fullShortUrl} " +
-            "    AND gid = #{param.gid} " +
-            "    AND create_time BETWEEN #{param.startDate} and #{param.endDate} " +
-            "GROUP BY " +
-            "    full_short_url, gid;")
-    LinkAccessStatsDO findPvUvUidStatsByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
 
     /**
      * 获取用户信息是否新老访客
@@ -105,6 +106,52 @@ public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
     );
 
     /**
+     * 获取分组用户信息是否新老访客
+     */
+    @Select("<script> " +
+            "SELECT " +
+            "    user, " +
+            "    CASE " +
+            "        WHEN MIN(create_time) BETWEEN #{startDate} AND #{endDate} THEN '新访客' " +
+            "        ELSE '老访客' " +
+            "    END AS uvType " +
+            "FROM " +
+            "    t_link_access_logs " +
+            "WHERE " +
+            "    gid = #{gid} " +
+            "    AND user IN " +
+            "    <foreach item='item' index='index' collection='userAccessLogsList' open='(' separator=',' close=')'> " +
+            "        #{item} " +
+            "    </foreach> " +
+            "GROUP BY " +
+            "    user;" +
+            "    </script>"
+    )
+    List<Map<String, Object>> selectGroupUvTypeByUsers(
+            @Param("gid") String gid,
+            @Param("startDate") String startDate,
+            @Param("endDate") String endDate,
+            @Param("userAccessLogsList") List<String> userAccessLogsList
+    );
+
+    /**
+     * 根据短链接获取指定日期内PV、UV、UIP数据
+     */
+    @Select("SELECT " +
+            "    COUNT(user) AS pv, " +
+            "    COUNT(DISTINCT user) AS uv, " +
+            "    COUNT(DISTINCT ip) AS uip " +
+            "FROM " +
+            "    t_link_access_logs " +
+            "WHERE " +
+            "    full_short_url = #{param.fullShortUrl} " +
+            "    AND gid = #{param.gid} " +
+            "    AND create_time BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    full_short_url, gid;")
+    LinkAccessStatsDO findPvUvUidStatsByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
+
+    /**
      * 根据分组获取指定日期内PV、UV、UIP数据
      */
     @Select("SELECT " +
@@ -119,24 +166,4 @@ public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
             "GROUP BY " +
             "    gid;")
     LinkAccessStatsDO findPvUvUidStatsByGroup(@Param("param") ShortLinkGroupStatsReqDTO requestParam);
-
-    /**
-     * 根据分组获取指定日期内高频访问IP数据
-     */
-    @Select("SELECT " +
-            "    ip, " +
-            "    COUNT(ip) AS count " +
-            "FROM " +
-            "    t_link_access_logs " +
-            "WHERE " +
-            "    gid = #{param.gid} " +
-            "    AND create_time BETWEEN #{param.startDate} and #{param.endDate} " +
-            "GROUP BY " +
-            "    gid, ip " +
-            "ORDER BY " +
-            "    count DESC " +
-            "LIMIT 5;")
-    List<HashMap<String, Object>> listTopIpByGroup(@Param("param") ShortLinkGroupStatsReqDTO requestParam);
-
-    List<Map<String, Object>> selectGroupUvTypeByUsers(String gid, String startDate, String endDate, List<String> userAccessLogsList);
 }
